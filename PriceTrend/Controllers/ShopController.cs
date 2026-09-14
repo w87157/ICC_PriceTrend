de789355fc06cb3aa2a28753e18270721faf6829
Kuan-Chih <a0909277719@gmail.com>
2026-09-14 20:06:33 +0800

    搜尋結果新增分頁功能

--- PriceTrend/Controllers/ShopController.cs
+++ PriceTrend/Controllers/ShopController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using PriceTrend.Services;
+using System.Linq;
 
 namespace PriceTrend.Controllers
 {
@@ -12,11 +13,27 @@ namespace PriceTrend.Controllers
             _searchService = searchService;
         }
 
-        public IActionResult Index(string? keyword)
+        public IActionResult Index(string? keyword, int page=1)
         {
+            int pageSize = 60;
+
             var products = _searchService.Search(keyword);
 
-            return View(products);
+            int totalCount = products.Count;
+
+            var pageProducts = products
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            ViewBag.CurrentPage = page;
+
+            ViewBag.TotalPages =
+                (int)Math.Ceiling(totalCount / (double)pageSize);
+
+            ViewBag.Keyword = keyword;
+
+            return View(pageProducts);
         }
     }
 }
